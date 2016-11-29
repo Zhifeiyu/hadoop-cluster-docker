82cf86eda0481b467864692ca98b4d7d14f4b7d4
root <root@localhost.localdomain>
2016-11-29 16:53:02 +0800

    add volume

--- start-container.sh
+++ start-container.sh
@@ -2,18 +2,21 @@
 
 # the default node number is 3
 N=${1:-3}
-
+HADOOP_IAMGES_NAME=zfylin/hadoop:1.0
+NET_NAME=zfylin_hadoop
+VOLUMN_PATH=/data/zfylin/hadoop-cluster
 
 # start hadoop master container
 sudo docker rm -f hadoop-master &> /dev/null
 echo "start hadoop-master container..."
 sudo docker run -itd \
-                --net=hadoop \
+                --net=${NET_NAME} \
                 -p 50070:50070 \
                 -p 8088:8088 \
                 --name hadoop-master \
                 --hostname hadoop-master \
-                kiwenlau/hadoop:1.0 &> /dev/null
+		-v ${VOLUMN_PATH}/hadoop-master/hdfs:/root/hdfs \
+                ${HADOOP_IAMGES_NAME} &> /dev/null
 
 
 # start hadoop slave container
@@ -23,10 +26,11 @@ do
 	sudo docker rm -f hadoop-slave$i &> /dev/null
 	echo "start hadoop-slave$i container..."
 	sudo docker run -itd \
-	                --net=hadoop \
+			--net=${NET_NAME} \
 	                --name hadoop-slave$i \
-	                --hostname hadoop-slave$i \
-	                kiwenlau/hadoop:1.0 &> /dev/null
+			--hostname hadoop-slave$i \
+			-v ${VOLUMN_PATH}/hadoop-slave$i/hdfs:/root/hdfs \
+			${HADOOP_IAMGES_NAME} &> /dev/null
 	i=$(( $i + 1 ))
 done 
 
